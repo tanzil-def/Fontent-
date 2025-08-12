bacf1780187a95f98ae2c40dce8ec4866c2b0fd7
sadiaprova12 <sadiaprova51717@gmail.com>
2025-08-12 18:39:31 +0600

    remove add new book

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -86,7 +86,7 @@ export default function Home() {
                 <Section title="Recommended" books={books.recommended} />
                 <Section title="Popular" books={books.popular} />
                 <NewBookCollections />
-                <BookSlider />
+                {/* <BookSlider /> */}
               </>
             ) : (
               <>
