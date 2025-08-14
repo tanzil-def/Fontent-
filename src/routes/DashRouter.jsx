25ec5221ae84fb11e7de4866f0a63ef256a58117
sadiaprova12 <sadiaprova51717@gmail.com>
2025-08-14 13:45:31 +0600

    auth protected remove files

--- src/routes/DashRouter.jsx
+++
@@ -1,9 +0,0 @@
-// // src/routes/DashRouter.jsx
-// import { Navigate } from "react-router-dom";
-
-// export default function DashRouter() {
-//   const user = JSON.parse(localStorage.getItem("user") || "null");
-//   if (user?.role === "admin") return <Navigate to="/admin" replace />;
-//   if (user?.role === "user") return <Navigate to="/app" replace />;
-//   return <Navigate to="/" replace />;
-// }
